b310af21b542842b247e7880d1c2a263d919293d
Usbak23 <amubarok1998@gmail.comm>
2025-05-10 06:58:22 +0700

    edit .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,29 @@ import "./globals.css"
 
 export default function Home() {
   return (
-    <main className="p-8 space-y-8">
-      <h1 className="text-2xl font-bold">Dashboard Customer</h1>
+    <>
+      <div className="bg-gray-200 p-4">
 
-      <section >
-        <h2 className="text-xl font-semibold mb-2">Data Customer</h2>
-        <CustomerTable />
-      </section>
+        <main className="p-8 space-y-8">
+          <h1 className="text-2xl font-bold">Dashboard Customer</h1>
 
-      <section>
-        <h2 className="text-xl font-semibold mb-2">Distribusi Gender</h2>
-        <div className="min-w-xs bg-blue-200 ">
-          <GenderChart />
-        </div>
-      </section>
-    </main>
+          <section >
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold mb-2">Data Customer</h2>
+              <CustomerTable />
+            </div>
+          </section>
+
+          <section>
+            <div>
+              <h2 className="text-xl font-semibold mb-2">Distribusi Gender</h2>
+              <div className="min-w-xs bg-blue-200 ">
+                <GenderChart />
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </>
   )
 }
